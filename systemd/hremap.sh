59fe9f980fd681551c3603004984627830082d0d
IMAI Hiroei <hre.bz.i@gmail.com>
2018-04-09 10:54:41 +0900

    サンプルスクリプトの更新

--- systemd/hremap.sh
+++ systemd/hremap.sh
@@ -3,10 +3,11 @@
 ### Keyboard devices
 DEV=
 for d in \
+    /dev/input/by-id/usb-ErgoDox_EZ_ErgoDox_EZ_0-event-kbd \
+    /dev/input/by-id/usb-ErgoDox_EZ_ErgoDox_EZ_0-if03-event-kbd \
+    /dev/input/by-id/usb-ErgoDox_EZ_ErgoDox_EZ-event-kbd \
     /dev/input/by-id/usb-PFU_Limited_HHKB_Professional_JP-event-kbd \
     /dev/input/by-id/usb-Topre_Corporation_RealForce_Compact-event-kbd \
-    /dev/input/by-id/usb-ErgoDox_EZ_ErgoDox_EZ_0-event-kbd \
-    /dev/input/by-id/usb-ErgoDox_EZ_ErgoDox_EZ-event-kbd \
     /dev/input/by-path/platform-i8042-serio-0-event-kbd \
     /dev/input/event2
 do
@@ -21,7 +22,7 @@ if [ ! -z "$DEV" ]; then
         *) ;;
     esac
     while true; do
-	/usr/local/bin/hremap --enable-ctrl-map $DEV
+	/usr/local/bin/hremap --enable-function-map --enable-ctrl-map --jp-to-us --caps-to-ctrl $DEV
 	sleep 2
     done
 else
