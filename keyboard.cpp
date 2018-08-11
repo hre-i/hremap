05c986952a0d51aeb922fbe2358dca64e916ff38
IMAI Hiroei <hre.bz.i@gmail.com>
2018-08-12 04:49:28 +0900

    map ScrLock to CapsLock

--- keyboard.cpp
+++ keyboard.cpp
@@ -98,6 +98,12 @@ bool KeyboardDevice::getKey(struct input_event* key)
 
     if (g_hhk_jp_to_us) {
         switch (key->code) {
+        case KEY_SCROLLLOCK:
+            key->code = KEY_CAPSLOCK;
+            DP(("-> %ld\tin : type %d, code %3d, value %d (%d) @%ld.%ld\n",
+                            time(NULL), key->type, key->code, key->value, (int)len,
+                            key->time.tv_sec, key->time.tv_usec));
+            break;
         case KEY_GRAVE:
             key->code = KEY_MUHENKAN;
             DP(("-> %ld\tin : type %d, code %3d, value %d (%d) @%ld.%ld\n",
